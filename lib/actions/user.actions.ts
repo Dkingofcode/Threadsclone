fd6c3f25e34b55ead2d611cb508057e5afa4493f
David <doladepo128@gmail.com>
2023-08-18 02:44:23 +0100

    finished the activity page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -129,7 +129,32 @@ export async function fetchUsers({
 }
 
 
+export async function getActivity(userId: string) {
+   try{
+     connectToDB();
 
+     // find all threads created by the user
+     const userThreads = await Thread.find({ author: userId });
+
+     // Collect all the child thread ids (replies) from the 'children' field
+     const childThreadsIds = userThreads.reduce((acc, userThread) => {
+        return acc.concat(userThread.children)
+     }, [])
+
+     const replies = await Thread.find({
+        _id: { $in: childThreadsIds },
+        author: { $ne: userId }
+     }).populate({
+        path: 'author',
+        model: User,
+        select: 'name image _id'
+     })
+
+     return replies;
+   } catch (error: any) {
+    throw new Error(`failed to fetch activity: ${error.message}`);
+   }
+}
 
 
 
